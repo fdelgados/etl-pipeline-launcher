9b2a103413337fc3da19d17e222f133745623fa4
Cisco Delgado <fdelgados@gmail.com>
2021-08-13 11:43:47 +0200

    Add expiration time to access token

--- src/identityaccess/infrastructure/service/token_generator.py
+++ src/identityaccess/infrastructure/service/token_generator.py
@@ -1,4 +1,5 @@
 import jwt
+import datetime
 
 from shared.infrastructure.application.settings import Settings
 
@@ -7,13 +8,19 @@ from identityaccess.domain.model.user import UserDescriptor
 
 
 class JwtTokenAuthentication(TokenGenerator):
+    _EXPIRATION_TIME_IN_HOURS = 6
+
     TOKEN_TYPE = "Bearer"
 
     def generate(self, user: UserDescriptor) -> str:
+        current_time = datetime.datetime.utcnow()
+
         payload = {
             "sub": user.tenant_id.value,
             "username": user.username,
-            "role": user.role
+            "role": user.role,
+            "iat": current_time,
+            "exp": current_time + datetime.timedelta(hours=self._EXPIRATION_TIME_IN_HOURS)
         }
 
         token = jwt.encode(payload, Settings.secret_key(), algorithm="HS256")
