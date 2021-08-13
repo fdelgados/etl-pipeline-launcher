a467f9b3f57f59fc19c9ca9c7b1f357d178a569f
Cisco Delgado <fdelgados@gmail.com>
2021-08-13 12:00:20 +0200

    Change quotation

--- src/identityaccess/infrastructure/service/token_generator.py
+++ src/identityaccess/infrastructure/service/token_generator.py
@@ -10,19 +10,19 @@ from identityaccess.domain.model.user import UserDescriptor
 class JwtTokenAuthentication(TokenGenerator):
     _EXPIRATION_TIME_IN_HOURS = 6
 
-    TOKEN_TYPE = "Bearer"
+    TOKEN_TYPE = 'Bearer'
 
     def generate(self, user: UserDescriptor) -> str:
         current_time = datetime.datetime.utcnow()
 
         payload = {
-            "sub": user.tenant_id.value,
-            "username": user.username,
-            "role": user.role,
-            "iat": current_time,
-            "exp": current_time + datetime.timedelta(hours=self._EXPIRATION_TIME_IN_HOURS)
+            'sub': user.tenant_id.value,
+            'username': user.username,
+            'role': user.role,
+            'iat': current_time,
+            'exp': current_time + datetime.timedelta(hours=self._EXPIRATION_TIME_IN_HOURS)
         }
 
-        token = jwt.encode(payload, Settings.secret_key(), algorithm="HS256")
+        token = jwt.encode(payload, Settings.secret_key(), algorithm='HS256')
 
         return token.decode('utf-8')
