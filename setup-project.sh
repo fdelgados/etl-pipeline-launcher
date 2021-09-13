d20a50d07517353ac73b8db39a1d64cf238f990f
Cisco Delgado <fdelgados@gmail.com>
2021-09-13 09:23:20 +0200

    Rename config file

--- setup-project.sh
+++ setup-project.sh
@@ -7,3 +7,11 @@ git config core.hooksPath resources/git/hooks
 echo $'#/bin/sh\n\nblack ./\n\npython lint.py --path ./src ./tests ./bin\n\nmake test' > resources/git/hooks/pre-commit
 
 chmod a+x resources/git/hooks/pre-commit
+
+# Install development requirements
+pip install virtualenv
+virtualenv -p python3 .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements-devel.txt
