27a9d6594be0fbfc2afce6416eb96f2c8ba6e1cf
Cisco Delgado <fdelgados@gmail.com>
2021-09-13 09:02:27 +0200

    Commit new config-git file

--- config-git.sh
+++ config-git.sh
@@ -6,4 +6,7 @@ git config core.hooksPath resources/git/hooks
 
 echo $'#/bin/sh\n\nblack ./\n\npython lint.py --path ./src ./tests ./bin\n\nmake test' > resources/git/hooks/pre-commit
 
-chmod a+x .git/hooks/pre-commit
+chmod a+x resources/git/hooks/pre-commit
+
+git add resources/git/hooks/pre-commit
+git commit -nm "Crate pre-commit hook"
