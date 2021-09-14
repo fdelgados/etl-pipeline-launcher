de946884c0b54d8018d03c1f1a4ccd29055bdfff
Cisco Delgado <fdelgados@gmail.com>
2021-09-14 09:47:24 +0200

    Create similarity threshold value object

---
+++ src/duplicates/shared/domain/model/similarity_threshold.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+
+class SimilarityThreshold:
+    _MIN_VALUE = 0
+    _MAX_VALUE = 1.0
+
+    def __init__(self, value: float):
+        self._ensure_is_valid_threshold(value)
+        self._value = value
+
+    def _ensure_is_valid_threshold(self, value):
+        if not isinstance(value, float):
+            raise ValueError(f"{value} is not a valid similarity threshold")
+
+        if value < self._MIN_VALUE:
+            raise ValueError(f"Similarity threshold cannot be lower than {self._MIN_VALUE}")
+
+        if value > self._MAX_VALUE:
+            raise ValueError(f"Similarity threshold cannot be higher than {self._MAX_VALUE}")
+
+    @staticmethod
+    def min() -> float:
+        return SimilarityThreshold._MIN_VALUE
+
+    @staticmethod
+    def max() -> float:
+        return SimilarityThreshold._MAX_VALUE
+
+    @property
+    def value(self) -> float:
+        return self._value
+
+    def __eq__(self, other: SimilarityThreshold) -> bool:
+        if not isinstance(other, SimilarityThreshold):
+            return False
+
+        return other.value == self._value
---
+++ tests/unit/duplicates/shared/domain/model/similarity_threshold_test.py
@@ -0,0 +1,33 @@
+import unittest
+from parameterized import parameterized
+from hypothesis import given, strategies as st
+
+from duplicates.shared.domain.model.similarity_threshold import SimilarityThreshold
+
+
+class SimilarityThresholdTest(unittest.TestCase):
+    @parameterized.expand(
+        ["a string", (10, ), (0o10, ), (0x10, )]
+    )
+    def test_if_it_raises_an_exception_when_type_is_wrong(self, value) -> None:
+        with self.assertRaises(ValueError):
+            SimilarityThreshold(value)
+
+    def test_if_it_raises_an_exception_when_value_is_lower_than_accepted(self) -> None:
+        with self.assertRaises(ValueError):
+            SimilarityThreshold(SimilarityThreshold.min() - 0.1)
+
+    def test_if_it_raises_an_exception_when_value_is_higher_than_accepted(self) -> None:
+        with self.assertRaises(ValueError):
+            SimilarityThreshold(SimilarityThreshold.max() + 0.1)
+
+    @given(st.floats(min_value=SimilarityThreshold.min(), max_value=SimilarityThreshold.max()))
+    def test_if_it_not_raise_an_exception_when_value_is_valid(self, value: float) -> None:
+        similarity_threshold = SimilarityThreshold(value)
+
+        self.assertTrue(
+            similarity_threshold == SimilarityThreshold(value),
+            f"{similarity_threshold.value} should be equals to {value}"
+        )
+
+
