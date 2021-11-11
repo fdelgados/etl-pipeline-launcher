9ad984422e71ea1843f033c5bb9460b335e6b3f6
Cisco Delgado <fdelgados@gmail.com>
2021-11-11 20:32:36 +0100

    Change setup project file

--- setup-project.sh
+++ setup-project.sh
@@ -10,17 +10,15 @@ if [ ! -z "$MODIFIED_FILES" ]
 then
     black $MODIFIED_FILES
     flake8 $MODIFIED_FILES
-    make unit-tests
+    if [ $? -eq 0 ]
+    then
+        make unit-tests
+    else
+        exit 1
+    fi
 fi'
 
 echo "$PRE_COMMIT_HOOK" > resources/git/hooks/pre-commit
 
 chmod a+x resources/git/hooks/pre-commit
 
-# Install development requirements
-pip install virtualenv
-virtualenv -p python3 .venv
-source .venv/bin/activate
-
-pip install --upgrade pip
-pip install -r requirements-devel.txt
