4ed037f822c71c3d7fb7f33cc6581bf3c55d0522
Cisco Delgado <fdelgados@gmail.com>
2021-12-15 16:03:48 +0100

    Retrieve pages in chunks

--- src/duplicates/data/infrastructure/datagatherer/mongodb/datagatherer.py
+++ src/duplicates/data/infrastructure/datagatherer/mongodb/datagatherer.py
@@ -10,7 +10,22 @@ from duplicates.data.domain.model.page import Page
 from duplicates.data.domain.service.datagatherer import DataGatherer
 
 
+def _add_pages(documents: list, pages: List[Page]) -> None:
+    for document in documents:
+        content = [section for _, section in document.get("content").items()]
+
+        pages.append(
+            Page(
+                Url(document.get("address")),
+                " ".join(content),
+                document.get("datalayer"),
+            )
+        )
+
+
 class DataGathererImpl(DataGatherer):
+    _PAGE_SIZE = 1000
+
     def __init__(self):
         connect_settings = global_vars.settings.mongodb_connection_settings()
 
@@ -25,32 +40,53 @@ class DataGathererImpl(DataGatherer):
         )
 
     def gather(self, corpus: str, corpus_build_id: str) -> List[Page]:
-        pages_collection = self._client[self._database][corpus]
         pages = []
+        last_id = None
 
-        documents = pages_collection.find(
+        while True:
+            documents, last_id = self._retrieve_documents(
+                corpus,
+                corpus_build_id,
+                last_id,
+            )
+
+            if documents is None and last_id is None:
+                break
+
+            _add_pages(documents, pages)
+
+        return pages
+
+    def _retrieve_documents(
+        self,
+        corpus: str,
+        corpus_build_id: str,
+        last_id=None,
+    ):
+        pages_collection = self._client[self._database][corpus]
+
+        query = {
+            "is_indexable": {"$eq": True},
+            "status_code": {"$eq": 200},
+            "build_id": {"$eq": corpus_build_id},
+        }
+
+        if last_id is not None:
+            query["_id"] = {"$gt": last_id}
+
+        cursor = pages_collection.find(
+            query,
             {
-                "is_indexable": {"$eq": True},
-                "status_code": {"$eq": 200},
-                "build_id": corpus_build_id,
-            },
-            {
-                "_id": 0,
                 "address": 1,
                 "content": 1,
             },
-        )
+        ).limit(self._PAGE_SIZE)
 
-        for document in documents:
-            content = [
-                section for _, section in document.get("content").items()
-            ]
-            pages.append(
-                Page(
-                    Url(document.get("address")),
-                    " ".join(content),
-                    document.get("datalayer"),
-                )
-            )
+        documents = [document for document in cursor]
 
-        return pages
+        if not documents:
+            return None, None
+
+        last_id = documents[-1]["_id"]
+
+        return documents, last_id
